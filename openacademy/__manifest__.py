fd3913f6013c0675b86b98290e6ba866698333e7
stanly2182 <42159021+stanly2182@users.noreply.github.com>
2020-07-22 08:28:15 -0600

    [CHANGE] sintaxis de __manifest__.py

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -25,9 +25,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views.xml',
-        'openacademy.xml',
+        'security/ir.model.access.csv',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [

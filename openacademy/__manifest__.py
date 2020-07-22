23233cd1bfc2ca2513de393175195bc9684e64a9
stanly2182 <42159021+stanly2182@users.noreply.github.com>
2020-07-22 15:54:03 -0600

    [CHANGE] course.xml y openacademy.xml

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -26,6 +26,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'views/openacademy.xml',
         'views/course.xml',
         'views/session.xml',
         'views/partner.xml',

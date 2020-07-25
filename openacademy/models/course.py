a505de0a9c773a59c483f6a53f69bd8b0c39d67c
stanly2182 <42159021+stanly2182@users.noreply.github.com>
2020-07-25 04:38:10 -0600

    [CHANGE] course.py

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -29,9 +29,5 @@ class Course(models.Model):
         default['name'] = new_name
         return super(Course, self).copy(default)
 
-    _sql_constraints = [
-        ('name_description_check', 'CHECK(name != description)',
-         "The title of the course should not be the description"),
-
-        ('name_unique', 'UNIQUE(name)', "The course title must be unique"),
-    ]
+    _sql_constraints = [('name_description_check', 'CHECK(name != description)', "The title of the course should not be the description"),
+                        ('name_unique', 'UNIQUE(name)', "The course title must be unique"), ]

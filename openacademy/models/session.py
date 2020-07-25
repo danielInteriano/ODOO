06a0e2b661a48f766f15ee7eee050852baa46809
stanly2182 <42159021+stanly2182@users.noreply.github.com>
2020-07-25 12:14:42 -0600

    [ADD] Advanced View-Graph View

--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -20,8 +20,12 @@ class Session(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
-    end_date = fields.Date(string="End Date", store=True,
-                           compute='_get_end_date', inverse='_set_end_date')
+
+    end_date = fields.Date(
+        string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+
+    attendees_count = fields.Integer(
+        string="Attendees count", compute='_get_attendees_count', store=True)
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -68,6 +72,11 @@ class Session(models.Model):
                 # Compute the difference between dates, but: Friday - Monday = 4 days,
                 # so add one day to get 5 days instead
                 r.duration = (r.end_date - r.start_date).days + 1
+
+        @api.depends('attendee_ids')
+        def _get_attendees_count(self):
+            for r in self:
+                r.attendees_count = len(r.attendee_ids)
 
         @api.constrains('instructor_id', 'attendee_ids')
         def _check_instructor_not_in_attendees(self):
